364ce8e12a90508b6caf9b80607ffbc14503fb82
NTNhan09 <ntnhan14789@gmail.com>
2022-01-10 15:58:46 +0700

    themes

---
+++ Employee.GUI/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace Employee.GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvHR = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbDb = new System.Windows.Forms.DateTimePicker();
+            this.CbSex = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbPb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbIdD = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbIdE = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(619, 344);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 80;
+            this.button4.Text = "Thoát";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(538, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 79;
+            this.button3.Text = "Sửa";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(457, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 78;
+            this.button2.Text = "Xoá";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(376, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 77;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dgvHR
+            // 
+            this.dgvHR.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHR.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dgvHR.Location = new System.Drawing.Point(12, 58);
+            this.dgvHR.Name = "dgvHR";
+            this.dgvHR.Size = new System.Drawing.Size(683, 280);
+            this.dgvHR.TabIndex = 76;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Mã";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Họ tên";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Ngày sinh";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Giới tính";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Nơi sinh";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Đơn vị";
+            this.Column6.Name = "Column6";
+            // 
+            // tbDb
+            // 
+            this.tbDb.Location = new System.Drawing.Point(397, 6);
+            this.tbDb.Name = "tbDb";
+            this.tbDb.Size = new System.Drawing.Size(200, 20);
+            this.tbDb.TabIndex = 75;
+            // 
+            // CbSex
+            // 
+            this.CbSex.AutoSize = true;
+            this.CbSex.Location = new System.Drawing.Point(608, 8);
+            this.CbSex.Name = "CbSex";
+            this.CbSex.Size = new System.Drawing.Size(89, 17);
+            this.CbSex.TabIndex = 74;
+            this.CbSex.Text = "Giới tính nam";
+            this.CbSex.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(337, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 73;
+            this.label5.Text = "Ngày sinh";
+            // 
+            // tbPb
+            // 
+            this.tbPb.Location = new System.Drawing.Point(215, 32);
+            this.tbPb.Name = "tbPb";
+            this.tbPb.Size = new System.Drawing.Size(480, 20);
+            this.tbPb.TabIndex = 72;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(170, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 71;
+            this.label3.Text = "Nơi sinh";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(215, 6);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 20);
+            this.tbName.TabIndex = 70;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(170, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 69;
+            this.label4.Text = "Họ tên";
+            // 
+            // tbIdD
+            // 
+            this.tbIdD.Location = new System.Drawing.Point(50, 32);
+            this.tbIdD.Name = "tbIdD";
+            this.tbIdD.Size = new System.Drawing.Size(100, 20);
+            this.tbIdD.TabIndex = 68;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Đơn vị";
+            // 
+            // tbIdE
+            // 
+            this.tbIdE.Location = new System.Drawing.Point(50, 6);
+            this.tbIdE.Name = "tbIdE";
+            this.tbIdE.Size = new System.Drawing.Size(100, 20);
+            this.tbIdE.TabIndex = 66;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 65;
+            this.label1.Text = "Mã";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 389);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvHR);
+            this.Controls.Add(this.tbDb);
+            this.Controls.Add(this.CbSex);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbPb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbIdD);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbIdE);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvHR;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DateTimePicker tbDb;
+        private System.Windows.Forms.CheckBox CbSex;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbPb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbIdD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbIdE;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
